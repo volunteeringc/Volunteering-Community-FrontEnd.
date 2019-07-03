eb54a9ef1f42848c4810ebef98a8b2981f0e0a3b
Lobna <loolysultan123@gmail.com>
2019-07-02 20:29:50 -0700

    1- import user class 2- import axios 3- declare variable that holds form data 4- add axios post method & api url

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NewUser} from '../../new-user';
+import axios from 'axios';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,8 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class SignUpComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit() {
   }
-
+   userModel = new  NewUser ("","","");
+  onSubmit(){
+    console.log(this.userModel);
+    axios.post("https://desolate-anchorage-26344.herokuapp.com/signup", {
+      email:this.userModel.newEmail,
+      password:this.userModel.newPassword,
+      name:this.userModel.newName
+    }).then(function(response) {
+      console.log("res:", response);
+    }).catch(function(err) {
+      console.log("error:", err);
+    })
+  }
 }
