5182c75004f3ff0b06b20f84a1a7dc34d157bb81
Lobna <loolysultan123@gmail.com>
2019-07-02 20:29:38 -0700

    1- import user class 2- import axios 3- declare variable that holds form data 4- add axios post method & api url

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../user';
+import axios from 'axios';
+
 
 @Component({
   selector: 'app-landing-page',
@@ -11,5 +14,18 @@ export class LandingPageComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  userModel = new User ("","")  ;
+  
+  onSubmit(){
+    console.log(this.userModel);
+    console.log(this.userModel);
+    axios.post("https://desolate-anchorage-26344.herokuapp.com/login ", {
+      email:this.userModel.Uemail,
+      password:this.userModel.Upassword,
+    }).then(function(response) {
+      console.log("res:", response);
+    }).catch(function(err) {
+      console.log("error:", err);
+    })
+  }
 }
