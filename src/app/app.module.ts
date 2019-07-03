71c647414d9db8a4ba41b598ef85727ee9b23769
Lobna <loolysultan123@gmail.com>
2019-07-02 20:24:13 -0700

    add Routes for land page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule , Routes} from '@angular/router'
+import { RouterModule , Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { LogInComponent } from './components/log-in/log-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 
-
  const appRoutes:Routes = [
-  {path:'',component:HomePageComponent},
+  {path:'',component:LandingPageComponent},
   {path:'home',component:HomePageComponent},
   {path:'profile',component:ProfilePageComponent},
   {path:'signup',component:SignUpComponent},
-  {path:'login',component:LogInComponent},
+  {path:'login',component:LandingPageComponent},
   
 ];
 
@@ -24,14 +23,14 @@ import { ProfilePageComponent } from './components/profile-page/profile-page.com
   declarations: [
     AppComponent,
     LandingPageComponent,
-    LogInComponent,
     SignUpComponent,
     HomePageComponent,
-    ProfilePageComponent
+    ProfilePageComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
