90040b096d1f021ff31e2b9aac94669bc6944343
Lobna <loolysultan123@gmail.com>
2019-07-02 21:41:31 -0700

    creat porject components / add reqired libary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LogInComponent,
+    SignUpComponent,
+    HomePageComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
